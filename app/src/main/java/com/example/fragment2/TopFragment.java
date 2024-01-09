0e8616b3a9b8a95f933c869e40f1d2a7e69da4ac
orikaskas <Orikaskas1234@gmail.com>
2024-01-09 15:07:07 +0200

    fragmentTest

--- app/src/main/java/com/example/fragment2/TopFragment.java
+++ app/src/main/java/com/example/fragment2/TopFragment.java
@@ -1,5 +1,6 @@
 package com.example.fragment2;
 
+import android.database.Cursor;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -18,6 +19,7 @@ public class TopFragment extends Fragment {
     View tmp;
     EditText editText ;
     Button btn;
+    MyDatabaseHelper myDatabaseHelper;
    public TopFragment() {
         // Required empty public constructor
     }
@@ -36,17 +38,17 @@ public class TopFragment extends Fragment {
     }
     public void init()
     {
+        myDatabaseHelper = new MyDatabaseHelper(requireActivity());
         editText = tmp.findViewById(R.id.etname);
         btn = tmp.findViewById(R.id.btnadd);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(editText.getText().length() ==0)
+                if(editText.getText().length() ==0||editText.length()>15)
                     Toast.makeText(getActivity(), "לא כתבת ", Toast.LENGTH_SHORT).show();
                 else {
-                    Bundle result = new Bundle();
-                    result.putString("name",editText.getText().toString());
-                    getParentFragmentManager().setFragmentResult("datafrom1",result);
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment2,new bottomfragment()).addToBackStack(null).commit();
+                    myDatabaseHelper.addproduct(editText.getText().toString(),0);
                     editText.setText("");
                 }
             }
--- app/src/main/java/com/example/fragment2/bottomfragment.java
+++ app/src/main/java/com/example/fragment2/bottomfragment.java
@@ -1,38 +1,31 @@
 package com.example.fragment2;
 
+import android.database.Cursor;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentResultListener;
 
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.LinearLayout;
+import android.widget.FrameLayout;
+import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Stack;
 
 
-public class bottomfragment extends Fragment implements View.OnClickListener {
+public class bottomfragment extends Fragment  {
     View tmp;
-    TableLayout tableLayout;
 
-    List<CheckBox> c1=new LinkedList<>();
-    List<TextView> name2 = new LinkedList<>();
+
+    MyDatabaseHelper myDatabaseHelper1;
+    Cursor cursor;
+    TableLayout t;
+    int n2;
 
 
     public bottomfragment() {
@@ -47,51 +40,69 @@ public class bottomfragment extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         tmp= inflater.inflate(R.layout.fragment_bottomfragment, container, false);
-        init();
-        return tmp;
-    }
-    public void init()
-    {
-         tableLayout = tmp.findViewById(R.id.Tlbottom);
 
-
-         getParentFragmentManager().setFragmentResultListener("datafrom1", this, new FragmentResultListener() {
-             @Override
-             public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
-                 TableRow t1 = new TableRow(getActivity());
-                 TableLayout.LayoutParams params = new TableLayout.LayoutParams(-1,50);
-                 params.setMargins(60,0,60,0);
-                 t1.setLayoutParams(params);
-                 TextView textView = new TextView(getActivity());
-                 CheckBox c = new CheckBox(getActivity());
-                 c1.add(c);
-                 name2.add(textView);
-                 textView.setTextColor(Color.BLACK);
-                 c.setOnClickListener(bottomfragment.this::onClick);
-                 TableRow.LayoutParams params1= new TableRow.LayoutParams(100,50);
-                 params1.setMargins(98,0,0,0);
-                 textView.setLayoutParams(params1);
-                 textView.setText(result.getString("name"));
-                 t1.addView(c);
-                 t1.addView(textView);
-                 t1.setBackground(getResources().getDrawable(R.drawable.tablec));
-                 tableLayout.addView(t1);
-             }
-         });
-    }
-    @Override
-    public void onClick(View v) {
-        for (int j = 0; j < c1.size(); j++)
-        {
-            if (c1.get(j)==v) {
-                if (c1.get(j).isChecked())
-                {
-                    name2.get(j).setTextColor(Color.BLUE);
-                }
-                else
-                    name2.get(j).setTextColor(Color.BLACK);
-            }
+        myDatabaseHelper1 = new MyDatabaseHelper(requireActivity());
+        cursor = myDatabaseHelper1.readAllData();
+        t=tmp.findViewById(R.id.Tlbottom);
+        int n =cursor.getCount();
+        cursor.moveToFirst();
+        for (int i = 0; i < n; i++) {
+            String s = cursor.getString(1);
+            int p = cursor.getInt(2);
+            init(s,p, Integer.parseInt(cursor.getString(0)));
+            cursor.moveToNext();
         }
 
+        return tmp;
     }
-}
+    public void init(String s, int p, int i)
+    {
+
+            TableRow row = new TableRow(requireActivity());
+            row.setTag(i);
+            TableRow.LayoutParams rowp = new TableRow.LayoutParams(-2, -2);
+            row.setLayoutParams(rowp);
+            row.setBackgroundColor(Color.WHITE);
+            TextView tvName = new TextView(requireActivity());
+            tvName.setTag("name");
+            TableRow.LayoutParams ll = new TableRow.LayoutParams(-1, -2);
+            ll.setMargins(0, 1, 0, 0);
+            tvName.setLayoutParams(ll);
+            tvName.setTextColor(Color.BLACK);
+            tvName.setTextSize(15);
+            tvName.setGravity(Gravity.CENTER);
+            tvName.setText(s);
+            CheckBox tvPrice = new CheckBox(requireActivity());
+            tvPrice.setTag("Checked");
+            tvPrice.setLayoutParams(ll);
+            tvPrice.setTextColor(Color.BLACK);
+            tvPrice.setTextSize(15);
+            tvPrice.setGravity(Gravity.CENTER);
+            if (p == 1){
+                tvPrice.setChecked(true);
+                tvName.setTextColor(Color.BLUE);
+            }
+            else{
+                tvPrice.setChecked(false);
+
+                tvName.setTextColor(Color.BLACK);
+            }
+            tvPrice.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if ((tvPrice.isChecked())){
+                        myDatabaseHelper1.updateData(String.valueOf(i),1);
+                        tvName.setTextColor(Color.BLUE);
+                    }
+                    else{
+                        myDatabaseHelper1.updateData(String.valueOf(i),0);
+                        tvName.setTextColor(Color.BLACK);
+                    }
+
+                }
+            });
+            row.addView(tvPrice);
+            row.addView(tvName);
+            t.addView(row);
+        }
+    }
